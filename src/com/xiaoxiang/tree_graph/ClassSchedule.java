c91b2a7e4971e91b32ebfe8603d3eac068302d1b
lw <liang645669081@126.com>
2020-09-10 14:55:55 +0800

    新增课程表，暂时提交

---
+++ src/com/xiaoxiang/tree_graph/ClassSchedule.java
@@ -0,0 +1,19 @@
+package com.xiaoxiang.tree_graph;
+
+/**
+ * author:w_liangwei
+ * date:2020/9/10
+ * Description: 课程表 LeetCode 207
+ */
+public class ClassSchedule {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+
+
+        return false;
+    }
+}
