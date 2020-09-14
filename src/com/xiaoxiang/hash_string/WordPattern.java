26781cc664a47aee52fa6d83074156aa5beae9e7
lw <liang645669081@126.com>
2020-09-14 19:41:27 +0800

    暂时提交

---
+++ src/com/xiaoxiang/hash_string/WordPattern.java
@@ -0,0 +1,17 @@
+package com.xiaoxiang.hash_string;
+
+/**
+ * author:w_liangwei
+ * date:2020/9/14
+ * Description:
+ */
+public class WordPattern {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean wordPattern(String pattern, String s) {
+
+        return false;
+    }
+}
