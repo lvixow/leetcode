e6766b9f269b51f1973fb7fda1b2ad5706b6b207
lw <liang645669081@126.com>
2020-09-15 19:39:02 +0800

    修改注释内容

--- src/com/xiaoxiang/hash_string/FindRepeatedDnaSequences.java
+++ src/com/xiaoxiang/hash_string/FindRepeatedDnaSequences.java
@@ -21,9 +21,10 @@ public class FindRepeatedDnaSequences {
 
     public static List<String> findRepeatedDnaSequences(String s) {
         int len = s.length();
-        //用于比较是否出现过重复
+        //用于比较是否出现过重复，使用set集合不会超时可能和底层使用哈希结构实现有关，如果使用list当LeetCode传一个超大字符串时造成超时
+        //使用contains方法查询元素是否存在HashSet要比ArrayList快的多。ArrayList的contains拿出数组中的所有元素一一比较，而hashSet底层使用hashmap的哈希定位到链表然后取值
         Set<String> temp = new HashSet<>();
-        //用于存放最终结果
+        //用于存放最终结果，由于可能会出现重复结果，所以需要用set去重
         Set<String> res = new HashSet<>();
         //遍历所有长度为10的子字符串
         for (int i = 0; i <= len - 10; i++) {
