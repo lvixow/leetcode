41edd4985a17b952854eb49e7af0cd2eb9d7afcf
645669081 <645669081@qq.com>
2020-09-18 23:25:00 +0800

    修复岛屿数量数组越界问题

--- src/com/xiaoxiang/search/LandNumber.java
+++ src/com/xiaoxiang/search/LandNumber.java
@@ -7,12 +7,13 @@ package com.xiaoxiang.search;
  */
 public class LandNumber {
     public static void main(String[] args) {
-//        char[][] grid = {{'1','1','0','0','0'},
-//                        {'1','1','0','0','0'},
-//                        {'0','0','1','0','0'},
-//                        {'0','0','0','1','1'}};
+        char[][] grid = {{'1','1','0','0','0'},
+                        {'1','1','0','0','0'},
+                        {'0','0','1','0','0'},
+                        {'0','0','0','1','1'}};
 
-        char[][] grid = {{'1'}, {'1'}};
+//        char[][] grid = {{'1'},
+//                         {'1'}};
         int numIslands = numIslands(grid);
         System.out.println(numIslands);
     }
@@ -24,11 +25,11 @@ public class LandNumber {
         //0代表水域，1代表岛屿，2代表岛屿并已经访问过
         //记录岛屿数量
         int count = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid.length; j++) {
+        for (int row = 0; row < grid.length; row++) {
+            for (int column = 0; column < grid[0].length; column++) {
                 //如果当前位置是岛屿，并且未访问过，我们就访问。执行完本地dfs则相连的一片区域全部被搜索完成
-                if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
+                if (grid[row][column] == '1') {
+                    dfs(grid, row, column);
                     count++;
                 }
             }
@@ -36,21 +37,21 @@ public class LandNumber {
         return count;
     }
 
-    private static void dfs(char[][] grid, int x, int y) {
+    private static void dfs(char[][] grid, int row, int column) {
         //越界了直接跳出本次递归
-        if (x < 0 || x >= grid[0].length || y < 0 || y >= grid.length) {
+        if (row < 0 || row >= grid.length || column < 0 || column >= grid[0].length) {
             return;
         }
         //访问过了该节点或是水域位置则跳出本次递归
-        if (grid[x][y] != '1') {
+        if (grid[row][column] != '1') {
             return;
         }
 
         //标记当前位置为已访问，并访问四周的位置
-        grid[x][y] = '2';
-        dfs(grid, x+1, y);
-        dfs(grid, x, y+1);
-        dfs(grid, x-1, y);
-        dfs(grid, x, y-1);
+        grid[row][column] = '2';
+        dfs(grid, row+1, column);
+        dfs(grid, row, column+1);
+        dfs(grid, row-1, column);
+        dfs(grid, row, column-1);
     }
 }
