abb608bec774a53a31ecd3a79258378ebaafd904
lw <liang645669081@126.com>
2020-09-24 16:54:05 +0800

    新增接雨水2，暂时提交

---
+++ src/com/xiaoxiang/search/TrapRainWater.java
@@ -0,0 +1,22 @@
+package com.xiaoxiang.search;
+
+/**
+ * author:w_liangwei
+ * date:2020/9/24
+ * Description: 收集雨水 LeetCode 407
+ */
+public class TrapRainWater {
+    public static void main(String[] args) {
+        int[][] heightMap = {{1,4,3,1,3,2},
+                            {3,2,1,3,2,4},
+                            {2,3,3,2,3,1}};
+        int rainWater = trapRainWater(heightMap);
+        System.out.println(rainWater);
+    }
+
+    public static int trapRainWater(int[][] heightMap) {
+
+
+        return 0;
+    }
+}
