64bfcef7cca188ccb1decfbdf76a4665335f375b
lw <liang645669081@126.com>
2020-09-24 14:32:03 +0800

    火柴拼正方形完成

--- src/com/xiaoxiang/search/MatchSquare.java
+++ src/com/xiaoxiang/search/MatchSquare.java
@@ -9,10 +9,14 @@ import java.util.Arrays;
  */
 public class MatchSquare {
     public static void main(String[] args) {
-        int[] nums = {3,3,3,3,4};
+//        int[] nums = {3,3,3,3,4};
+//        int[] nums = {1,1,2,2,2};
+        int[] nums = {10,10,10,10,7,7,7,7,7,7,7,7,6,6,6,6};
         boolean makesquare = makesquare(nums);
         System.out.println(makesquare);
     }
+
+    private static boolean ans = false;
 
     /**
      * @auther 梁伟
@@ -29,7 +33,7 @@ public class MatchSquare {
         for (int num : nums) {
             total = total + num;
         }
-        if (total % 4 != 0) {
+        if (nums.length < 4 || total % 4 != 0) {
             return false;
         }
         //排序为了每次可以拿出最大的边，方便构建正方形
@@ -50,21 +54,27 @@ public class MatchSquare {
      * @Param [nums, target, temp, index] 总火柴，单边长度，每边的总长度，当前要加入的火柴在nums中的索引
      * @return boolean
      **/
-    private static boolean backtrack(int[] nums, int target, int[] temp, int index) {
-        //当所有边都满足时返回
-        if (temp[0] == target && temp[1] == target && temp[2] == target && temp[3] == target) {
-            return true;
+    private static boolean backtrack(int[] nums, int target, int[] bucket, int index) {
+        //当所有火柴放完，所有边满足长度要求
+        if (index == -1) {
+            return bucket[0] == target && bucket[1] == target && bucket[2] == target && bucket[3] == target;
         }
-        if (index > nums.length -1) {
-            return false;
-        }
-        //加入当前火柴
-        for (int i = 0; i < temp.length; i++) {
-            if (temp[i] + nums[index] <= target) {
-                temp[i] = temp[i] + nums[index];
-                backtrack(nums, target, temp, index -1);
+        //尝试从每条边加入当前火柴
+        for (int i = 0; i < 4; i++) {
+            //只有当加入该火柴后不超过边长才能加入
+            if (bucket[i] + nums[index] > target) {
+                continue;
             }
+            //加入火柴
+            bucket[i] = bucket[i] + nums[index];
+            //当返回true，说明所有火柴放完了，并且也组成了正方形，此时应该停止摆放，也不需要回溯
+            if (backtrack(nums, target, bucket, index -1)) {
+                return true;
+            }
+            //当前火柴摆放的边不合适，重新摆放
+            bucket[i] = bucket[i] - nums[index];
         }
-        return backtrack(nums, target, temp, index);
+        //上一根摆放的不合适，重新摆放
+        return false;
     }
 }
