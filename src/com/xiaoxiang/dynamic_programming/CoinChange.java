55c68f7e71f7ef3c87604652aff4ea4c79fef95b
lw <liang645669081@126.com>
2020-09-27 15:04:49 +0800

    优化逻辑，更加易于理解

--- src/com/xiaoxiang/dynamic_programming/CoinChange.java
+++ src/com/xiaoxiang/dynamic_programming/CoinChange.java
@@ -1,5 +1,7 @@
 package com.xiaoxiang.dynamic_programming;
 
+import java.util.Arrays;
+
 /**
  * author:w_liangwei
  * date:2020/9/25
@@ -7,33 +9,33 @@ package com.xiaoxiang.dynamic_programming;
  */
 public class CoinChange {
     public static void main(String[] args) {
-        int[] coins = {1, 2, 5};
-//        int[] coins = {2};
-        int coinChange = coinChange(coins, 11);
+//        int[] coins = {1, 2, 5};
+        int[] coins = {2};
+        int coinChange = coinChange(coins, 3);
         System.out.println(coinChange);
     }
 
     public static int coinChange(int[] coins, int amount) {
         //初始化dp，dp[i]代表金额为i时的最优解
         int[] dp = new int[amount + 1];
-        for (int i = 0; i < dp.length; i++) {
-            dp[i] = -1;
-        }
+        //填充dp的默认值为amount + 1,因为最小的输入面额为1元，所以当前金额所需的最大钞票张数就是该金额都是被1元组成的，而amount + 1就是一个不可能的解，从而来判断该金额是否有最优解
+        Arrays.fill(dp, amount + 1);
         //金额0的最优解是0
         dp[0] = 0;
         //获得从1到i之间所有的最优解，从而推出i的最优解
         for (int i = 1; i <= amount; i++) {
             //获取不同面值下的最优解，逐渐更新最优解，找到最小值
             for (int j = 0; j < coins.length; j++) {
-                //i-coins[j]保证当前金额至少比一张面额的金额大（也保证了索引不越界） 并且 当前最优解所依赖的上一个的最优解已经计算出来
-                if (i-coins[j] >= 0 && dp[i-coins[j]] != -1) {
-                    if (dp[i] == -1 || dp[i] > dp[i-coins[j]] + 1) {
-                        //递推方程：dp[i] = dp[i-coins[j]] +1。即当前金额的最优解 = 选择一张钞票 + 剩余面额的最优解，如 10 = (2)1 + dp[8], 10 = (5)1 + dp[5]
-                        dp[i] = dp[i-coins[j]] + 1;
-                    }
+                //当前金额没有该钞票面额大，则无法组成该金额，跳过该钞票
+                if (i - coins[j] < 0) {
+                    continue;
                 }
+                //递推方程：dp[i] = dp[i-coins[j]] +1。即当前金额的最优解 = 选择一张钞票 + 剩余面额的最优解，如 10 = (2)1 + dp[8], 10 = (5)1 + dp[5]
+                //更新最优解
+                dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
             }
         }
-        return dp[amount];
+        //当dp[i]是初始化给的不可能的值时，说明没有最优解。返回-1，否则返回最优解
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 }
