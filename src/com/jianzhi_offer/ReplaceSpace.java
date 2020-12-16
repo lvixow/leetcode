7fd6090dba6f72db6e2de57ab451e8a1d3b28e0f
lw <liang645669081@126.com>
2020-12-16 17:31:22 +0800

    新增offer05

---
+++ src/com/jianzhi_offer/ReplaceSpace.java
@@ -0,0 +1,14 @@
+package com.jianzhi_offer;
+
+/**
+ * author:w_liangwei
+ * date:2020/12/16
+ * Description: offer 05
+ */
+public class ReplaceSpace {
+    public static void main(String[] args) {
+        String s = "We are happy.";
+        String replace = s.replace(" ", "%20");
+        System.out.println(replace);
+    }
+}
