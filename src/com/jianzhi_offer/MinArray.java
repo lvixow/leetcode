deb1b7d9409c641131d836daf989b2ae0a49ec5a
lw <liang645669081@126.com>
2020-12-21 16:09:58 +0800

    新增offer11

--- src/com/jianzhi_offer/MinArray.java
+++ src/com/jianzhi_offer/MinArray.java
@@ -16,8 +16,11 @@ public class MinArray {
         while (left < right) {
             int mid = (left + right) / 2;
             if (arr[mid] > arr[right]) {
+                //如果中间值大于最右边的值，说明旋转之后最小的数字肯定在mid的右边
                 left = mid + 1;
             } else if (arr[mid] < arr[right]) {
+                //如果中间值小于最右边的值，说明旋转之后最小的数字肯定在mid的前面
+                //注意这里mid是不能减1的，比如[3，1，3]，我们这里只是证明了numbers[mid]比numbers[right]小，但有可能numbers[mid]是最小的，也就是那个旋转点，所以我们不能把它给排除掉
                 right = mid;
             } else {
                 //如这样的数组[3，1，3，3，3，3，3]和[3，3，3，3，3，1，3]虽然相等，但无法确定旋转点，但是直接将右指针直接左移一位并不会舍去旋转点的值，因为此时mid处和right处
